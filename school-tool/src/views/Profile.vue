715c43181c70f0e902e5c1cf5efd82a34d34ea53
bms14 <brunooires14@gmail.com>
2020-12-30 18:45:58 +0000

    imagem e alguimas modificações

--- school-tool/src/views/Profile.vue
+++ school-tool/src/views/Profile.vue
@@ -1,45 +1,57 @@
 <template>
   <div id="profile">
+    <h1>Perfil</h1>
+    <div id="info" class="border border-dark">
       <div id="name">
-          Nome: <br>
-          <p>{{getUser.name}}</p>
+        <h5>Nome</h5>
+        <p>{{ getUser.name }}</p>
       </div>
       <div id="birthDate">
-          Data de nascimento: <br>
-          <p>{{getUser.birthDate}}</p>
+        <h5>Data de nascimento</h5>
+        <p>{{ getUser.birthDate }}</p>
       </div>
       <div id="email">
-          Email: <br>
-          <p>{{getUser.email}}</p>
+        <h5>Email</h5>
+        <p>{{ getUser.email }}</p>
       </div>
       <div id="course">
-          Curso: <br>
-          <p>{{getUser.course}}</p>
+        <h5>Curso</h5>
+        <p>{{ getUser.course }}</p>
       </div>
       <div id="foto">
-          <img width="200px" height="150px" :src="getUser.photo" /><br>
-          <input
-            type="file"
-            class="form-control-file"
-            id="profileImage"
-            name="img"
-            accept="image/*"
-          /> <br>
-          <button type="submit" class="btn btn-primary">
-          Editar
-        </button>
+        <img
+          width="150px"
+          height="150px"
+          :src="getUser.photo"
+          class="rounded-circle"
+        /><br />
+        <input
+          type="file"
+          class="form-control-file"
+          id="profileImage"
+          name="img"
+          accept="image/*"
+        />
+        <br />
+        <button type="submit" class="btn btn-primary">Editar</button>
       </div>
+    </div>
+    <br />
+    <div id="gamification" class="border border-dark">
+      <img width="35px" height="25px" src="../assets/console.png" />
+      <h3>Gamificação</h3>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-     name: 'Profile',
-    computed: {
-        getUser(){
-            return this.$store.getters.getLoggedUser
-        }
+  name: "Profile",
+  computed: {
+    getUser() {
+      return this.$store.getters.getLoggedUser;
     }
+  }
 };
 </script>
 
