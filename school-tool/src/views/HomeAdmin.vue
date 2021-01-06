6631c293185569cfd8192dd2edaaeb968579fd7c
bms14 <brunoires14@gmail.com>
2021-01-06 21:13:22 +0000

    cards admin

--- school-tool/src/views/HomeAdmin.vue
+++ school-tool/src/views/HomeAdmin.vue
@@ -1,9 +1,42 @@
 <template>
   <div id="adminHome">
     <h1>Admin</h1>
-    <p>Número de users: {{ getNumUsers }}</p>
-    <p>Número de Atividades: {{ getNumActivities }}</p>
-    <p>Número de Comentários: {{ getNumComments }}</p>
+    <h3 id="stats">Estatísticas Totais:</h3>
+    <div class="d-flex justify-content-center">
+      <div id="card" class="card" style="width: 18rem">
+        <h3>Users</h3>
+        <div class="card-body">
+          <img
+            class="card-img-top"
+            src="../assets/user.png"
+            style="width: 50px"
+          />
+          <h4 class="card-title">{{ getNumUsers }}</h4>
+        </div>
+      </div>
+      <div id="card" class="card" style="width: 18rem">
+        <h3>Comentários</h3>
+        <div class="card-body">
+          <img
+            class="card-img-top"
+            src="../assets/comment.png"
+            style="width: 50px"
+          />
+          <h4 class="card-title">{{ getNumComments }}</h4>
+        </div>
+      </div>
+      <div id="card" class="card" style="width: 18rem">
+        <h3>Atividades</h3>
+        <div class="card-body">
+          <img
+            class="card-img-top"
+            src="../assets/clipboard.png"
+            style="width: 50px"
+          />
+          <h4 class="card-title">{{ getNumActivities }}</h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -22,9 +55,17 @@ export default {
     },
     getNumComments() {
       return this.$store.getters.getNumComments;
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+#card {
+  top: 20px;
+  margin: auto;
+  margin-top: 2%;
+  border-radius: 30px;
+  box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.5);
+}
+</style>
