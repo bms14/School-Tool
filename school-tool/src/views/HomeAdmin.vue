34c73be1d97620bc37194463ac583f9da561cd69
bms14 <brunooires14@gmail.com>
2021-01-06 23:22:58 +0000

    commit

--- school-tool/src/views/HomeAdmin.vue
+++ school-tool/src/views/HomeAdmin.vue
@@ -4,6 +4,7 @@
     <h3 id="stats">Estatísticas Totais:</h3>
     <div class="d-flex justify-content-center">
       <div id="card" class="card" style="width: 18rem">
+        <br />
         <h3>Users</h3>
         <div class="card-body">
           <img
@@ -11,10 +12,13 @@
             src="../assets/user.png"
             style="width: 50px"
           />
-          <h4 class="card-title">{{ getNumUsers }}</h4>
+          <br /><br />
+          <h3 class="card-title">{{ getNumUsers }}</h3>
+          <button @click="seeUsers" class="btn btn-primary">Ver Users</button>
         </div>
       </div>
       <div id="card" class="card" style="width: 18rem">
+        <br />
         <h3>Comentários</h3>
         <div class="card-body">
           <img
@@ -22,10 +26,15 @@
             src="../assets/comment.png"
             style="width: 50px"
           />
-          <h4 class="card-title">{{ getNumComments }}</h4>
+          <br /><br />
+          <h3 class="card-title">{{ getNumComments }}</h3>
+          <button @click="seeActivities" class="btn btn-primary">
+            Ver Atividades
+          </button>
         </div>
       </div>
       <div id="card" class="card" style="width: 18rem">
+        <br />
         <h3>Atividades</h3>
         <div class="card-body">
           <img
@@ -33,7 +42,11 @@
             src="../assets/clipboard.png"
             style="width: 50px"
           />
-          <h4 class="card-title">{{ getNumActivities }}</h4>
+          <br /><br />
+          <h3 class="card-title">{{ getNumActivities }}</h3>
+          <button @click="seeComments" class="btn btn-primary">
+            Ver Comentários
+          </button>
         </div>
       </div>
     </div>
@@ -57,6 +70,17 @@ export default {
       return this.$store.getters.getNumComments;
     },
   },
+  methods: {
+    seeUsers() {
+      this.$router.push({ name: "AdminListUser" });
+    },
+    seeActivities() {
+      this.$router.push({ name: "AdminListActivity" });
+    },
+    seeComments() {
+      this.$router.push({ name: "AdminListComments" });
+    },
+  },
 };
 </script>
 
@@ -67,5 +91,11 @@ export default {
   margin-top: 2%;
   border-radius: 30px;
   box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.5);
+}
+#card .btn {
+  font-size: 15px;
+  background: #fec107;
+  color: #fff;
+  cursor: pointer;
 }
 </style>
