62921be3df6f43060172d4c93995657cd12ea362
catarinafonseca <catarinaconstantef@gmail.com>
2021-01-06 11:15:16 +0000

    icon do forum correto

--- school-tool/src/components/Navbar.vue
+++ school-tool/src/components/Navbar.vue
@@ -16,7 +16,7 @@
       <router-link to="/profile"><ion-icon name="person-outline"></ion-icon> Perfil</router-link> |
       <router-link to="/addActivity"><ion-icon name="clipboard-outline"></ion-icon> Adicionar atividade</router-link> |
       <router-link :to='{name: "ListActivity"}'><ion-icon name="home-outline"></ion-icon> Catálogo</router-link> |
-      <router-link to="/forum"><ion-icon name="clipboard-outline"></ion-icon> Forum</router-link> 
+      <router-link to="/forum"><ion-icon name="chatbubbles-outline"></ion-icon> Forum</router-link> 
   </div>
 </template>
 
