09fb85ee3005eaa556bc887283f4f7dbfbfd7892
catarinafonseca <catarinaconstantef@gmail.com>
2021-01-07 00:15:54 +0000

    ok

--- school-tool/src/views/Profile.vue
+++ school-tool/src/views/Profile.vue
@@ -10,13 +10,14 @@
             :src="getUser.photo"
             class="rounded-circle"
           /><br />
+          <div >
           <input
             type="link"
             class="input"
             v-model="photo"
             placeholder="Insira um novo link"
           />
-          <button type="submit" class="btn">Atualizar</button>
+          <button type="submit" class="btn">Atualizar</button></div>
         </form>
       </div>
       <br />
@@ -71,7 +72,33 @@
       </div>
     </div>
     <div id="gamification">
-      <h1>Gamificação</h1>
+      <h1><ion-icon name="game-controller-outline"></ion-icon> Gamificação</h1>
+      <p>Tipo de perfil: <span>Iniciante</span> <button id="infobtn"><ion-icon name="information-circle-outline"></ion-icon></button></p>
+      <p>Pontos: <span>1</span><button id="infobtn"><ion-icon name="information-circle-outline"></ion-icon></button></p>
+      <p>Conquistas: <span>3</span><button id="infobtn"><ion-icon name="information-circle-outline"></ion-icon></button></p>
+      <p>Certificados: <span>2</span><button id="infobtn"><ion-icon name="information-circle-outline"></ion-icon></button></p>
+    </div> <button type="button" class="btn btn-outline-danger" id="logout" data-toggle="modal"
+                    data-target="#modalLogout">Logout</button>
+    <div class="modal" id="modalLogout" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Logout</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Tens a certeza que queres sair?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Não</button>
+                    <a href="">
+                        <button type="button" id="btnLogout" class="btn btn-danger">Sim, tenho!</button>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -129,7 +156,7 @@ export default {
   text-align: center;
 }
  #profile #info #line,
-#oneline h5, #oneline p {
+#oneline h5, #oneline p, #gamification p {
   text-align: left;
 } 
 #profile #pass .btn,
@@ -139,13 +166,35 @@ export default {
   margin-left: 7px;
   padding: 8px 0px;
   font-size: 15px;
-  background: #fec107;
-  color: #fff;
+  border-color: #fec107;
+  border-width: 2px;
   cursor: pointer;
   border-radius: 3px;
   outline: none;
 }
-
+#profile #pass .btn:hover,
+#interests .btn:hover,
+#foto .btn:hover {
+  width: 90px;
+  margin-left: 7px;
+  padding: 8px 0px;
+  font-size: 15px;
+  background-color: #fec107;
+  color:#fff;
+  cursor: pointer;
+  border-radius: 3px;
+  outline: none;
+}
+#infobtn{
+  background: none;
+  border: none;
+}
+#gamification ion-icon {
+  color: #fec107;
+  top: 4px ;
+  position: relative;
+  
+}
 
 #profile #interests input[type="radio"]:checked:after {
   width: 15px;
