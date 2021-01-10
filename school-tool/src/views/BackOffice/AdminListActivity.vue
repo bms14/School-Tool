3133c48695b889e329326e46613511d8a1fa24f6
bms14 <brunooires14@gmail.com>
2021-01-10 19:17:28 +0000

    commit

--- school-tool/src/views/BackOffice/AdminListActivity.vue
+++ school-tool/src/views/BackOffice/AdminListActivity.vue
@@ -1,21 +1,78 @@
 <template>
   <div id="adminListActivities">
-    <h1>Admin</h1>
-    <p>Número de Atividades: {{ getNumActivities }}</p>
+    <h1>Atividades</h1>
+    <div id="listActivities">
+      <div v-if="getNumActivities == 0">NÃO EXISTEM ATIVIDADES</div>
+      <div v-else class="row justify-content-center">
+        <div id="filterActivities">
+          <label for="txtFilterName">NOME:</label>
+          <input type="text" v-model="filterName" id="txtFilterName" />
+        </div>
+        <br />
+        <br />
+        <table>
+          <tr v-for="activity in filteredActivities" :key="activity.id">
+            <td>
+              <img
+                width="80px"
+                height="80px"
+                :src="activity.photo"
+                class="rounded-circle"
+              />
+            </td>
+            <td>{{ activity.name }}</td>
+            
+
+            <td>
+              <button @click="seeActivity(activity.id)" class="btn btn-danger">
+                VER DETALHES
+              </button>
+              <button @click="removeActivity(activity.id)" class="btn btn-danger">
+                REMOVER
+              </button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script>
 export default {
   name: "AdminListActivities",
+  data() {
+    return {
+      filterName: "",
+    };
+  },
+  /* methods: {
+    seeActivity(id){
+
+    },
+    removeActivity(id){
+
+    }
+  }, */
   computed: {
     getUser() {
       return this.$store.getters.getLoggedUser;
     },
     getNumActivities() {
       return this.$store.getters.getNumActivities;
-    }
-  }
+    },
+
+    filteredActivities() {
+      if (!this.filterName) {
+        return this.$store.state.activities;
+      } else {
+        return this.$store.state.activities.filter((activity) =>
+          activity.name.includes(this.filterName)
+        );
+      }
+    },
+  },
 };
 </script>
 
