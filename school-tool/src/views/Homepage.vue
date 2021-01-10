81abf04d506867dd5806c6eaef9f0492717a970d
catarinafonseca <catarinaconstantef@gmail.com>
2021-01-10 19:15:31 +0000

    ok

--- school-tool/src/views/Homepage.vue
+++ school-tool/src/views/Homepage.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="homepage">
+  <div id="homepage" class="container">
     <!-- <h1>Authenticaded area</h1>
       <h2>Hello {{getUser}}</h2> -->
 
    
-  <div>
+  
  <b-carousel
     id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
+    style="text-shadow: 0px 0px 2px #000; width:100%"
     fade
     indicators
-    img-width="1600"
-    img-height="480"
+    
   >
     <b-carousel-slide
       
@@ -26,7 +25,7 @@
       img-src="https://picsum.photos/1600/480/?image=22"
     ></b-carousel-slide>
   </b-carousel>
-</div>
+
 
 
     <h1>O que te apetece fazer hoje?</h1>
