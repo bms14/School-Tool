3b404365038e45b0ab74fac1d8c4ffc7b3968072
jialn5210 <jialongye5210@gmail.com>
2021-01-23 15:21:08 +0000

    ..

--- school-tool/src/views/Activity.vue
+++ school-tool/src/views/Activity.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="content">
     <div v-if="activity">
-<<<<<<< HEAD
-      <b-button variant="light" type="button" @click="goBack"
-        >Retroceder</b-button
-      >
-=======
->>>>>>> 56e149e2673939ab8fd0b75436b1b6e7fa2c166e
       <b-container id="activity">
         <b-card class="activityCard">
           <b-button variant="light" class="goBack" type="button" @click="goBack"
@@ -46,7 +40,7 @@
               <p>{{ activity.numPeople }}</p>
             </div>
           </b-row>
-          <div v-if="subscripted = ''">
+          <div v-if="subscripted = 'false'">
             <b-button
               @click="newEnrollment(activity.id)"
               variant="outline-warning"
@@ -78,11 +72,7 @@ export default {
     return {
       activity: null,
       enrollments: this.$store.state.enrollments,
-<<<<<<< HEAD
       subscribed: "false",
-=======
-      subscribed: null,
->>>>>>> 56e149e2673939ab8fd0b75436b1b6e7fa2c166e
     };
   },
   created() {
@@ -126,7 +116,6 @@ export default {
       /* let enr = this.enrollments.filter(enrollment => enrollment.idUser === this.$store.getters.getLoggedUser.id)
        enr.filter(enrollment => 
            enrollment.idActivity != payload.id) 
-<<<<<<< HEAD
          */
           try {
         this.$store.dispatch("cancelEnrollment", {
@@ -140,11 +129,6 @@ export default {
       }
      /*  this.subscribed = "false";
       this.$store.dispatch("cancelEnrollment", payload); */
-=======
-        
-            this.subscribed = "false"; */
-      this.$store.dispatch("cancelEnrollment", payload);
->>>>>>> 56e149e2673939ab8fd0b75436b1b6e7fa2c166e
     },
   },
   /*  checkSubscription(payload){
