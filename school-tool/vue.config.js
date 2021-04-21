50c5e8b501b1eacef25e93d464868508a8bc4e30
catarinafonseca <catarinaconstantef@gmail.com>
2021-04-21 17:25:03 +0100

    tentativa1

--- school-tool/vue.config.js
+++ school-tool/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {   
-    publicPath: process.env.NODE_ENV === 'production'     
-    ? '/School-Tool/'     
-    : '/' }
+    css: {
+      modules: true,
+      extract: false
+    } }
